5b3737ca3cba2534c3f759cfe2d2202bf5d86177
knc <91279644+cuongkn@users.noreply.github.com>
2023-05-17 22:13:12 +0700

    demo

--- frontend/src/components/Chatbox/Chatbox.js
+++ frontend/src/components/Chatbox/Chatbox.js
@@ -10,7 +10,7 @@ import {
   MainContainer,
 } from "@chatscope/chat-ui-kit-react";
 
-const API_KEY = "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"
+const API_KEY = "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"
 
 const systemMessage = { 
   "role": "system", 
